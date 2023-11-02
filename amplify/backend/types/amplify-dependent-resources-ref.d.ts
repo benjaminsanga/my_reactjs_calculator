d1197210f84e8ff93c95d09a57b0929a4cbbba36
benjaminsanga <benjamin.sanga@outsourceglobal.com>
2023-11-02 18:08:40 +0100

    removed amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "myreactjscalculator": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
